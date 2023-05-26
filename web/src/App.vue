baea98f5f474d4cc7c223578156ecf73059c4637
Даша <ccslls368@gmail.com>
2023-05-26 11:02:02 +0500

    added CKEditor

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import News from "./components/News.vue";
+import Editor from "./components/Editor.vue";
 </script>
 
 <template>
   <News />
+  <Editor />
 </template>
 
 <style></style>
---
+++ web/src/components/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="editor">
+    <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+    ></ckeditor>
+  </div>
+</template>
+
+<script>
+//import CKEditor from "@ckeditor/ckeditor5-vue";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+export default {
+  name: "editor",
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: "<p>Rich-text editor content.</p>",
+      editorConfig: {
+        // The configuration of the rich-text editor.
+      },
+    };
+  },
+};
+</script>
